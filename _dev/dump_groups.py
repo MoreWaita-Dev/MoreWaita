32d8a13bab293ce5904a73b03f983546e23d06cd
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-19 15:30:09 +0800

    fix the batshit

--- _dev/dump_groups.py
+++ _dev/dump_groups.py
@@ -1,69 +1,98 @@
-#!/bin/python
-
-import os
 import argparse
-from graphlib import TopologicalSorter
 import re
+from os import listdir, readlink
+from os.path import isfile, join, islink
+from graphlib import TopologicalSorter
 
-parser = argparse.ArgumentParser(description='Update regular files and groups of links in meson.build file.')
-parser.add_argument('readdir', metavar='readdir', type=str, nargs='?', default='.',)
+
+parser = argparse.ArgumentParser(
+        description=(
+            "Update regular files and groups"
+            " of links in meson.build file."
+        )
+    )
+parser.add_argument(
+    "readdir",
+    metavar="readdir",
+    type=str,
+    nargs="?",
+    default="."
+)
 args = parser.parse_args()
 
+DIR: str = args.readdir
 # read all files from directory
-files = sorted(os.listdir(args.readdir))
-files.remove('meson.build')
+files = sorted(listdir(DIR))
+files.remove("meson.build")
 
-# filter out regular files
-regular_files = [
-    "    '{}',\n".format(file)
-    for file in files
-    if os.path.isfile(os.path.join(args.readdir, file)) and not os.path.islink(os.path.join(args.readdir, file))
-]
 
-# filter out link files
-link_files = [
-    file
-    for file in files
-    if os.path.isfile(os.path.join(args.readdir, file)) and os.path.islink(os.path.join(args.readdir, file))
-]
+regular_files: list[str] = []
+link_files: list[str] = []
+
+for file in files:
+    file_: str = join(DIR, file)
+    # filter out regular files
+    if isfile(join(file_)) and not islink(file_):
+        regular_files.append("    '{}',\n".format(file))
+    # filter out link files
+    elif isfile(join(file_)) and islink(file):
+        link_files.append(file)
 
 # dictonary for grouping files and their links
-groups = {}
-
-# for each link find a file it links to and add it to that group
+groups: dict = {}
 for link in link_files:
-    groups.setdefault(os.readlink(os.path.join(args.readdir, link)), []).append(link)
+    groups.setdefault(
+        readlink(
+            join(args.readdir, link)
+        ), []
+    ).append(link)
 
 # sort groups topologically
 # that way chain links will be created in correct order
 ts = TopologicalSorter(groups)
-# get a list of tuples so the order is maintained when pairing sorted keys with their values
-sorted_groups = [(file, groups[file]) for file in ts.static_order() if file in groups]
+
+# get a list of tuples so the order is maintained
+# when pairing sorted keys with their values
+sorted_groups: list[tuple[str, str]] = []
+for file in ts.static_order():
+    if file in groups:
+        sorted_groups.append((file, groups[file]))
 
 # iterate over all groups
-# reverse the order of groups so first the dependencies are created
+# reverse the order of groups so
+# first the dependencies are created
 new_link_lines = []
 for file, links in reversed(sorted_groups):
-    # print a comment line indicating a group of links (file that they link to)
-    new_link_lines.append("    '{}': [\n".format(file))
+    # print a comment line indicating a
+    # group of links (file that they link to)
+    new_link_lines.append("    "{}": [\n".format(file))
     for link in links:
         # print a line for each link file
-        new_link_lines.append("        '{}',\n".format(link))
+        new_link_lines.append("        "{}",\n".format(link))
     new_link_lines.append("    ],\n")
 
 reg_lines = None
 link_lines = None
 
 # match lines in regular and link segments
-r = re.compile(r'^((?:.*\n)+    # DO NOT REMOVE: Begining of regular segment\n)((?:.+\n)*)(    # DO NOT REMOVE: End of regular segment\n(?:.*\n)+    # DO NOT REMOVE: Begining of link segment\n)((?:.+\n)*)(    # DO NOT REMOVE: End of link segment\n(?:.*\n)+)$', re.MULTILINE)
+r = re.compile(
+    (
+        r"^((?:.*\n)+    # DO NOT REMOVE: Begining of reg"
+        r"ular segment\n)((?:.+\n)*)(    # DO NOT REMOVE:"
+        r" End of regular segment\n(?:.*\n)+    # DO NOT "
+        r"REMOVE: Begining of link segment\n)((?:.+\n)*)("
+        r"    # DO NOT REMOVE: End of link segment\n(?:.*\n)+)$"
+    ),
+    re.MULTILINE
+)
 
-with open(os.path.join(args.readdir, 'meson.build'), 'r') as f:
-    match_lines = r.match(''.join(f.readlines()))
+with open(join(args.readdir, "meson.build"), "r") as f:
+    match_lines = r.match("".join(f.readlines()))
 
 if match_lines:
-    with open(os.path.join(args.readdir, 'meson.build'), 'w') as f:
+    with open(join(args.readdir, "meson.build"), "w") as f:
         f.write(match_lines.group(1))
-        f.write(''.join(regular_files))
+        f.write("".join(regular_files))
         f.write(match_lines.group(3))
-        f.write(''.join(new_link_lines))
+        f.write("".join(new_link_lines))
         f.write(match_lines.group(5))
